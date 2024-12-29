a393b035ae35f5d5cad998a8def6c4f349b7e483
Toms-D-Barvidis <Toms-Deimonds.Barvidis@edu.rtu.lv>
2024-12-29 15:20:45 +0200

    fb

--- script.js
+++ script.js
@@ -5,37 +5,42 @@ function getValues() {
     var buzzValue = document.getElementById("buzz-value").value;
 
     console.log("Values saved");
+    const fizzBuzzDiv = document.getElementById("fizzbuzz-container");
 
-    for (let i = 1; i <= endValue; i++){
-        const fizzBuzzDiv = document.getElementById("fizzbuzz-container");
+    removeAllChildNodes(fizzBuzzDiv);
+
+    for (let i = startingValue; i <= endValue; i++){
         const newDiv = document.createElement("div");
-        if (i % (fizzValue * buzzValue) == 0){
+        if ((i % fizzValue == 0) && (i % buzzValue == 0)){
             newDiv.className = "fizzbuzz";
-        } else if (i % fizzValue == 0){
-                newDiv.className = "fizz"
-            } else if (i % buzzValue == 0) {
-                newDiv.className = "buzz"
-            } else {
-                newDiv.className = "nofizzbuzz"
-            };
+        } else if (i % fizzValue == 0) {
+            newDiv.className = "fizz";
+        } else if (i % buzzValue == 0) {
+            newDiv.className = "buzz";
+        } else {
+            newDiv.className = "nofizzbuzz";
+        }
         
         newDiv.innerHTML = i;
         fizzBuzzDiv.appendChild(newDiv);
     }
 }
 
-
-
-
-
-//     // iterate through the array and add cars
-//     for (let i =0; i <cars.length; i++){
-//         console.log("Car", cars[i]);
-//         // addToList(list_el, cars[i], "car")
-//         if (i % 2 === 0){
-//             addToList(list_el, cars[i], "even-car");
-//         } else
-//         addToList(list_el, cars[i], "odd-car");
-//     }
+function wipeAll() {
+    const fizzBuzzDiv = document.getElementById("fizzbuzz-container");
+    removeAllChildNodes(fizzBuzzDiv);
     
-// }
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function setDefaults(){
+    document.getElementById("starting-value").value = 1;
+    document.getElementById("end-value").value = 100;
+    document.getElementById("fizz-value").value = 3;
+    document.getElementById("buzz-value").value = 5;
+}
